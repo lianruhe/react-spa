5d0d74c621932d9709ce4e1c4866f55d027a125e
lianruhe <lianruhe2013@gmail.com>
2017-09-26 22:40:33 +0800

    [M] update iframe module

--- src/modules/iframe/index.jsx
+++ src/modules/iframe/index.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import propTypes from 'prop-types'
+import React, { PureComponent } from 'react'
+import Iframe from 'react-iframe'
+import { getUrlParam } from 'utils/tools'
 
-import MAP from './route-map'
+import { Spin } from 'antd'
 
 import 'styles/app/iframe.css'
 
-export default class Iframe extends React.Component {
-  static propTypes = {
-    location: propTypes.object.isRequired
-  }
-
+export default class IframeComponent extends PureComponent {
   render () {
-    const { pathname } = this.props.location
-    const src = MAP[pathname]
+    const paramUrl = getUrlParam('url')
+    const match = paramUrl.match(/^https?:\/\/[^/]+/) || []
+    const redirect = ({ origin, data }) => {
+      if (origin === match[0]) {
+        const { messageType, url } = data || {}
+        if (messageType === 'childMessageRedirect' && url) {
+          typeof url === 'number' ? window.history.go(url) : window.location.href = url
+        }
+      }
+    }
+    if (window.attachEvent) {
+      window.detachEvent('message', redirect)
+      window.attachEvent('message', redirect)
+    } else {
+      window.removeEventListener('message', redirect)
+      window.addEventListener('message', redirect)
+    }
 
     return (
-      <iframe src={src} id="extend-iframe" frameBorder="0">{this.props.location.pathname}连接服务器错误！</iframe>
+      <div id="iframe-wrapper">
+        <Spin tip="加载中..." size="large" />
+        {/* <iframe src={url} id="extend-iframe" frameBorder="0">{url}: 连接服务器错误！</iframe> */}
+        <Iframe
+          key={Date.now()}
+          url={paramUrl}
+          width="100%"
+          height="100%"
+          display="block"
+          position="absolute"
+          allowFullScreen />
+      </div>
     )
   }
 }
